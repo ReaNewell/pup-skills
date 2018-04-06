8c5154e654f0fbfe816ff9bfad245fd86f0aa19d
Rea Newell <patrick.r.newell@gmail.com>
2018-04-06 11:39:30 +0900

    added tests to dogs test file

--- src/tests/actions/dogs.test.js
+++ src/tests/actions/dogs.test.js
@@ -1,6 +1,22 @@
-import { activateDog, addDog, editDog, removeDog, setDogs } from "../../actions/dogs";
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { activateDog, addDog, editDog, removeDog, setDogs, startAddDog, startEditDog, startRemoveDog } from "../../actions/dogs";
 import dogs from '../fixtures/dogs';
+import database from '../../firebase/firebase';
 
+const uid = 'this is my test uid';
+const defaultAuthState = { auth: {uid} };
+const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+    const dogsData = {};
+    dogs.forEach(({ id, name, breed, isActive, skills }) => {
+        dogsData[id] = { name, breed, isActive, skills };
+    });
+    database.ref(`users/${uid}/dogs`).set(dogsData).then(() => done());
+})
+
+// ACTIVATE DOG
 test('should activate dog in action object', () => {
     const action = activateDog( dogs[0].id );
     expect(action).toEqual({
@@ -9,6 +25,7 @@ test('should activate dog in action object', () => {
     })
 });
 
+// ADD DOG
 test('should add dog in action object', () => {
     const action = addDog(dogs[0]);
     expect(action).toEqual({
@@ -16,7 +33,32 @@ test('should add dog in action object', () => {
         dog: dogs[0]
     })
 });
+test('should add dog to database and store', (done) => {
+    const store = createMockStore(defaultAuthState);
+    const dogData = {
+        name: 'Yolanda',
+        breed: 'Yorkie',
+        isActive: false,
+        skills: []
+    };
 
+    store.dispatch(startAddDog(dogData)).then(() => {
+        expect(action[0]).toEqual({
+            type: 'ADD_DOG',
+            dog: {
+                id: expect.any(String),
+                ...dogData
+            }
+        });
+        return database.ref(`users/${uid}/dogs/${actions[0].dog.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(dogData);
+        done();
+    }).catch(done);
+});
+
+
+// EDIT DOG
 test("should edit dog in action object", () => {
     const action = editDog(dogs[0].id, {breed: "Chihuahua"});
     expect(action).toEqual({
@@ -27,15 +69,54 @@ test("should edit dog in action object", () => {
         }
     })
 });
+test('should edit dog object in database and store', (done) => {
+    const store = createMockStore(defaultAuthState);
+    const updates = {
+        breed: 'Pitbull'
+    }
 
+    store.dispatch(startEditDog(dogs[0].id, updates)).then(() => {
+        expect(action[0]).toEqual({
+            type: 'EDIT_DOG',
+            id: dogs[0].id,
+            updates
+        })
+        return database.ref(`users/${uid}/dogs/${actions[0].dog.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual({
+            ...dogs[0],
+            updates
+        });
+        done();
+    }).catch(done);
+})
+
+
+//REMOVE DOG
 test("should remove dog in action object", () => {
     const action = removeDog(dogs[0].id);
     expect(action).toEqual({
         type: "REMOVE_DOG",
         id: dogs[0].id
-    })
+    });
+});
+test('should remove dog object from database and store', (done) => {
+    const store = createMockStore(defaultAuthState);
+
+    store.dispatch(startRemoveDog(dogs[1].id)).then(() => {
+        expect(action[0]).toEqual({
+            type: 'REMOVE_DOG',
+            id: dogs[1].id
+        });
+        return database.ref(`users/${uid}/dogs/${actions[0].dog.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toBeFalsy();
+        done();
+    }).catch(done);;
 });
 
+
+// SET DOGS
 test('should set dogs in action object', () => {
     const action = setDogs(dogs);
     expect(action).toEqual({
