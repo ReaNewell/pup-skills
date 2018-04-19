e0c12e98723c34f1492db51a20393bafcda58d79
Rea Newell <patrick.r.newell@gmail.com>
2018-04-19 13:31:09 +0900

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => (
+    <div className="footer">
+        <p>Website by Rea Newell</p>
+        <Link to='/help'>Privacy Policy and Help</Link>
+    </div>
+)
+
+export default Footer;
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export const PublicRoute = ({ isAuthenticated, component: Component, hasProfileInfo, ...rest }) => (
     <Route {...rest} component={(props) => (
@@ -10,6 +11,7 @@ export const PublicRoute = ({ isAuthenticated, component: Component, hasProfileI
                 <div>
                     <Header />
                     <Component {...props} />
+                    <Footer />
                 </div>
             ) : (
                 <Redirect to='/getting-started' />
@@ -18,6 +20,7 @@ export const PublicRoute = ({ isAuthenticated, component: Component, hasProfileI
             <div>
                     <Header />
                     <Component {...props} />
+                    <Footer />
             </div>
         )
     )}/>
