8e0131d6dd37f97cd62541a78ea7f08f9b7e4214
Rea Newell <patrick.r.newell@gmail.com>
2018-04-20 13:26:25 +0900

    email signup/login functionality, new login form/modal

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,19 +4,33 @@ export const login = (uid) => ({
     type: "LOGIN",
     uid
 });
-
 export const startLogin = () => {
     return () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
+    };
+};
+export const startLoginWithEmail = (email, password) => {
+    return () => {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
     };
 };
 
 export const logout = () => ({
     type: "LOGOUT"
 })
-
 export const startLogout = () => {
     return () => {
         return firebase.auth().signOut();
     };
+};
+
+export const signup = () => ({
+    type: "SIGNUP"
+});
+export const startSignUp = (email, password) => {
+    return () => {
+        return firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+            return firebase.auth().signInWithEmailAndPassword(email, password);
+        });
+    }
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,84 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { startLogout, startLogin } from '../actions/auth';
+import { startLogout, startLogin, startLoginWithEmail, startSignUp } from '../actions/auth';
 
-export const Header = ({ startLogout, startLogin, isAuthenticated }) => (
-    <header className="header">
-        <div className="content-container"> 
-            <div className="header__content">
-                <Link to='/dashboard' className="header__title">
-                    <h1 className="header__title">Pup<span>Skills</span></h1>
-                </Link>
-                <div>
-                    {isAuthenticated ? <button onClick={startLogout} className="header__button">Logout</button> : <button onClick={startLogin} className="header__button">Sign In</button>}
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            loginModalIsOpen: false,
+            password: ""
+        }
+    }
+    onEmailChange = (e) => {
+        const email = e.target.value;
+        this.setState(() => ({ email }));
+    }
+    onPasswordChange = (e) => {
+        const password = e.target.value;
+        this.setState(() => ({ password }));
+    }
+    openLoginModal = () => {
+        this.setState(() => ({ loginModalIsOpen: true }));
+    }
+    loginWithEmail = () => {
+        if (this.state.email && this.state.password) {
+            this.props.startLoginWithEmail(this.state.email, this.state.password);
+        }
+    }
+    signupWithEmail = () => {
+        if (this.state.email && this.state.password) {
+            this.props.startSignUp(this.state.email, this.state.password);
+        }
+    }
+    render() {
+        return (
+            <header className="header">
+                <div className="content-container"> 
+                    <div className="header__content">
+                        <Link to='/dashboard' className="header__title">
+                            <h1 className="header__title">Pup<span>Skills</span></h1>
+                        </Link>
+                        <div>
+                            {this.props.isAuthenticated ? (
+                                <button onClick={this.props.startLogout} className="header__button">Logout</button>
+                            ) : (
+                                <button onClick={this.openLoginModal} className="header__button">Sign In</button>
+                            )}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </header>
-);
+                {
+                    this.state.loginModalIsOpen && 
+                    <div className="login-modal">
+                        <div className="login-modal__box">
+                            <input 
+                                className="login-modal__email"
+                                onChange={this.onEmailChange}
+                                placeholder='Email'
+                                type='text'
+                                value={this.state.email}
+                            />
+                            <input 
+                                className="login-modal__password"
+                                onChange={this.onPasswordChange}
+                                placeholder='Password'
+                                type='text'
+                                value={this.state.password}
+                            />
+                            <button onClick={this.signupWithEmail} className="login-modal__button--signUp">Sign Up</button>
+                            <button onClick={this.loginWithEmail} className="login-modal__button--signIn">Login</button>
+                            <button onClick={this.props.startLogin} className="login-modal__button--signInGoogle">Sign In with Google</button>
+                        </div>
+                    </div>
+                }
+            </header>
+        )
+    }
+};
 
 const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
@@ -24,6 +86,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
     startLogout: () => dispatch(startLogout()),
-    startLogin: () => dispatch(startLogin())
+    startLogin: () => dispatch(startLogin()),
+    startSignUp: (email, password) => dispatch(startSignUp(email, password)),
+    startLoginWithEmail: (email, password) => dispatch(startLoginWithEmail(email, password))
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
