6717ceb4385c0645ebdf4a322f5904d6a56e6436
Rea Newell <reanewell@Reas-MacBook-Pro.local>
2018-11-20 09:15:07 -0600

    minor design changes to mobile skills list

--- src/components/mobile/MobileCompletedSkillsList.js
+++ src/components/mobile/MobileCompletedSkillsList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import selectSkills from '../../selectors/skillsByCategory';
 
 
@@ -17,27 +16,19 @@ export default class MobileCompletedSkillsList extends React.Component {
         this.setState({ completedSkills: selectSkills(nextProps.activeDog.skills, "Completed") });
     };
     render() {
-        const transition = {
-            transitionName: "skill-fade",
-            transitionEnterTimeout: 500,
-            transitionLeaveTimeout: 500
-        };
-
         return (
             <div className={this.props.listIsActive ? "skills-list" : "skills-list--inactive"}>
                 {   
                     this.state.completedSkills.length === 0 ? (
                         <p className="skills-list__message">There are no completed skills.</p>
                     ) : (
-                        <ReactCSSTransitionGroup {...transition}>
-                            {this.state.completedSkills.map((skill, index) => (
-                                    <SkillCard
-                                        key = {skill.id}
-                                        skill = {skill}
-                                    />
-                                
-                            ))}
-                        </ReactCSSTransitionGroup>
+                        this.state.completedSkills.map((skill, index) => (
+                                <SkillCard
+                                    key = {skill.id}
+                                    skill = {skill}
+                                />
+                            
+                        ))
                     )
                 }
             </div>
